bebddccbec0a8a5effd22be621139dd7e5e0c5a7
LABELNET <1406046087@qq.com>
2016-03-10 23:17:24 +0800

    【EDIT】实现 Token的本地存储与读取 方法

--- src/main/java/cn/ibona/qiniu_sdk/util/QiniuConstant.java
+++ src/main/java/cn/ibona/qiniu_sdk/util/QiniuConstant.java
@@ -4,5 +4,18 @@ package cn.ibona.qiniu_sdk.util;
  * Created by yuanmingzhuo on 16-3-10.
  * 放置常量
  */
-public class QiniuConstant {
+public interface QiniuConstant {
+
+
+
+    String SHARED_PREFENCE_TOKEN_KEY="shared_pref_token_key";  //shared_pref token key
+    String SHARED_PREFENCE_TOKEN_TIME_KEY="shared_pref_token_key"; //shared_pref token time
+    String SHARED_PREFENCE_TOKEN_DEFAULT="no";
+    String SHARED_PREFENCE_TOKEN_TIME_DEFAULT="2016-01-01 00:00:00";
+
+
+    String QINIU_DATE_UTIL_FORMAT="yyyy-MM-dd HH:mm:ss";
+
+
+
 }
--- src/main/java/cn/ibona/qiniu_sdk/util/QiniuSharedPref.java
+++ src/main/java/cn/ibona/qiniu_sdk/util/QiniuSharedPref.java
@@ -1,10 +1,52 @@
 package cn.ibona.qiniu_sdk.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import java.util.Date;
+
 /**
  * Created by yuanmingzhuo on 16-3-10.
  * 本地存储和读取Token
  * 同时存储系统时间
  */
-public class QiniuSharedPref {
+public final class QiniuSharedPref {
+
+
+    //初始化 SharedPreference
+    private static SharedPreferences sPref;
+    public static void init(Context cxt){
+        sPref=PreferenceManager.getDefaultSharedPreferences(cxt);
+    }
+
+
+    /**
+     * 存储token
+     * @param token 当前token
+     * @param date 当前时间
+     */
+    public static void setToken(String token,String date) {
+        SharedPreferences.Editor edit = sPref.edit();
+        edit.putString(QiniuConstant.SHARED_PREFENCE_TOKEN_KEY,token);
+        edit.putString(QiniuConstant.SHARED_PREFENCE_TOKEN_TIME_KEY,token);
+        edit.commit();
+    }
+
+    /**
+     * 获取 token
+     * @return
+     */
+    public String getToken() {
+        return sPref.getString(QiniuConstant.SHARED_PREFENCE_TOKEN_KEY,QiniuConstant.SHARED_PREFENCE_TOKEN_DEFAULT);
+    }
+
+    /**
+     * 获取时间
+     * @return
+     */
+    public String getDate(){
+        return sPref.getString(QiniuConstant.SHARED_PREFENCE_TOKEN_TIME_KEY,QiniuConstant.SHARED_PREFENCE_TOKEN_TIME_DEFAULT);
+    }
 
 }
