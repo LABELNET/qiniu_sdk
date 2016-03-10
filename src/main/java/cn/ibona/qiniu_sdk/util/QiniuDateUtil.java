f9ee3c9c1d918532a1b118857570206835706a87
LABELNET <1406046087@qq.com>
2016-03-10 23:20:13 +0800

    【ADD】添加日期时间解析方法,时间字符串转Date对象

--- src/main/java/cn/ibona/qiniu_sdk/util/QiniuDateUtil.java
+++ src/main/java/cn/ibona/qiniu_sdk/util/QiniuDateUtil.java
@@ -13,6 +13,11 @@ public class QiniuDateUtil {
 
     private static final SimpleDateFormat sdf=new SimpleDateFormat(QiniuConstant.QINIU_DATE_UTIL_FORMAT);
 
+    /**
+     * 2016-01-01 00:00:00 字符串 转 Date 对象
+     * @param date
+     * @return
+     */
     public static Date getDateByString(String date)  {
         try {
             return sdf.parse(date);
