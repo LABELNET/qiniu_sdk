0a910bacd64f7f40923b1436010eea8381b82b38
LABELNET <1406046087@qq.com>
2016-03-10 22:27:15 +0800

    【ADD】Gradle集成七牛SDK  创建 net和util包

---
+++ src/main/java/cn/ibona/qiniu_sdk/net/NetUrl.java
@@ -0,0 +1,10 @@
+package cn.ibona.qiniu_sdk.net;
+
+/**
+ * Created by yuanmingzhuo on 16-3-10.
+ * 存放 Url
+ */
+public interface NetUrl {
+
+    String TOKEN_URL="";
+}
---
+++ src/main/java/cn/ibona/qiniu_sdk/util/QiniuConstant.java
@@ -0,0 +1,7 @@
+package cn.ibona.qiniu_sdk.util;
+
+/**
+ * Created by yuanmingzhuo on 16-3-10.
+ */
+public class QiniuConstant {
+}
