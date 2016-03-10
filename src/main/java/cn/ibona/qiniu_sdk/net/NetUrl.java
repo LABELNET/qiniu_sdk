2fa3a0c0ac6214acb70f40382cfc6257738d391d
LABELNET <1406046087@qq.com>
2016-03-10 22:33:32 +0800

    【ADD】添加TokenBean

--- src/main/java/cn/ibona/qiniu_sdk/net/NetUrl.java
+++ src/main/java/cn/ibona/qiniu_sdk/net/NetUrl.java
@@ -2,11 +2,12 @@ package cn.ibona.qiniu_sdk.net;
 
 /**
  * Created by yuanmingzhuo on 16-3-10.
- * 防止 Url
+ * 放置 Url
  */
 public interface NetUrl {
 
     String TOKEN_URL="http://182.254.228.211:9000/index.php/Api/auth/token";
+
 
 
 
---
+++ src/main/java/cn/ibona/qiniu_sdk/net/bean/TokenBean.java
@@ -0,0 +1,8 @@
+package cn.ibona.qiniu_sdk.net.bean;
+
+/**
+ * Created by yuanmingzhuo on 16-3-10.
+ * token 实体
+ */
+public class TokenBean {
+}
