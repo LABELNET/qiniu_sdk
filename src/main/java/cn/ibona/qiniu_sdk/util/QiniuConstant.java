5ef7c5a9612e5684d1f9149820d14e1659e9d1bc
LABELNET <1406046087@qq.com>
2016-03-11 11:13:06 +0800

    【EDIT】修改 sharedPref 存储的时间 的 key

--- src/main/java/cn/ibona/qiniu_sdk/util/QiniuConstant.java
+++ src/main/java/cn/ibona/qiniu_sdk/util/QiniuConstant.java
@@ -9,7 +9,7 @@ public interface QiniuConstant {
 
 
     String SHARED_PREFENCE_TOKEN_KEY="shared_pref_token_key";  //shared_pref token key
-    String SHARED_PREFENCE_TOKEN_TIME_KEY="shared_pref_token_key"; //shared_pref token time
+    String SHARED_PREFENCE_TOKEN_TIME_KEY="shared_pref_token_time_key"; //shared_pref token time
     String SHARED_PREFENCE_TOKEN_DEFAULT="no";
     String SHARED_PREFENCE_TOKEN_TIME_DEFAULT="2016-01-01 00:00:00";
 
