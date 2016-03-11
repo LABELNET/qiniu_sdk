214a54f7c99a225edbccc7dfc546d799620ade27
LABELNET <1406046087@qq.com>
2016-03-11 12:40:29 +0800

    【EDIT】QiniuCallback -> 添加上传回调方法

--- src/main/java/cn/ibona/qiniu_sdk/net/QiniuCallback.java
+++ src/main/java/cn/ibona/qiniu_sdk/net/QiniuCallback.java
@@ -3,5 +3,24 @@ package cn.ibona.qiniu_sdk.net;
 /**
  * Created by yuanmingzhuo on 16-3-11.
  */
-public class QiniuCallback {
+public abstract class QiniuCallback {
+
+    /**
+     * 上传成功
+     * @param key 图片名称
+     */
+    public abstract void onSuccess(String key);
+
+    /**
+     * 上传失败
+     * @param msg 错误信息
+     */
+    public abstract void onError(String msg);
+
+    /**
+     * 当前进度
+     * @param percent 进度值
+     */
+    public void onProcess(double percent){}
+
 }
